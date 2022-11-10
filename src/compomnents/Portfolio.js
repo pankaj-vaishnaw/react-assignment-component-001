2c42896dace58fb05e220e7f1be5a4db6e1b6013
Pankaj Vaishnav <97958495+pankaj-vaishnaw@users.noreply.github.com>
2022-11-10 13:57:28 +0530

    Add files via upload

---
+++ src/compomnents/Portfolio.js
@@ -0,0 +1,58 @@
+import React from 'react'
+
+const Portfolio = () => {
+  return (
+    <div>
+      <div>
+        <h2>Ankur Verma</h2>
+        <h4>Software Developer </h4>
+        <a href="_blank">
+          Linkedin 
+        </a>
+        <a href="_blank">
+           GitHub
+        </a>
+        <hr />
+      </div>
+      <div>
+        <h3>Experience</h3>
+        <h4>Software Developer</h4>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo vitae vel
+          quae illum fuga id culpa aliquid consectetur corrupti. Ullam
+          doloremque aperiam maiores iste perferendis laborum corrupti pariatur
+          reprehenderit numquam illum! Sunt unde hic dignissimos reprehenderit
+          molestias fugit natus nostrum!
+        </p>
+        <hr />
+        <p>Software Developer</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo vitae vel
+          quae illum fuga id culpa aliquid consectetur corrupti. Ullam
+          doloremque aperiam maiores iste perferendis laborum corrupti pariatur
+          reprehenderit numquam illum! Sunt unde hic dignissimos reprehenderit
+          molestias fugit natus nostrum!
+        </p>
+        <hr />
+        <h2>Skills</h2>
+        <ul>
+          <li>Java</li>
+          <li>JavaScript</li>
+          <li>DSA</li>
+          <li>React</li>
+        
+        </ul>
+        <hr />
+        <h2>Hobbies</h2>
+        <ol>
+        
+          <li>Reading Books</li>
+          <li>Biography</li>
+          <li>Teaching</li>
+        </ol>
+      </div>
+    </div>
+  )
+}
+
+export default Portfolio
